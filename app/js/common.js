26aaae2623f1d5cef034bbb339cb3c44049872ca
Olya Zaritska <blackskater23@gmail.com>
2019-06-17 08:45:20 +0300

    top-sites test
    test task

---
+++ app/js/common.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+  var $menu = $("#menu");
+
+  $(window).scroll(function(){
+      if ( $(this).scrollTop() > 100 && $menu.hasClass("default") ){
+          $menu.removeClass("default").addClass("fixed");
+      } else if($(this).scrollTop() <= 100 && $menu.hasClass("fixed")) {
+          $menu.removeClass("fixed").addClass("default");
+      }
+  });
+});
